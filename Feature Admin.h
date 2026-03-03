8011fdae364de2f4bf4aba8e952a9c85f996efda
OsamaAdel178 <osamdel2006@gmail.com>
2026-03-03 04:10:36 +0200

    Add Admin class for managing employees and clients

---
+++ Feature Admin.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Employee.h"
+#include "Client.h"
+#include "FilesHelper.h"
+#include <vector>
+
+class Admin : public Employee
+{
+   static int nextAdminID;
+
+public:
+    Admin(const string& name, const string& password, double salary);
+    Admin(int id,const string& name, const string& password, double salary);
+
+    void display_info() const;
+    void addClient(Client& client);
+    Client* searchClient(int id);
+    void listClient();
+    void editClient(int id, string name, string password, double balance);
+    void addEmployee(Employee& employee);
+    Employee* searchEmployee(int id);
+    void editEmployee(int id, string name, string password, double salary);
+    void listEmployee();
+};
