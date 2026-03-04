75eb77e7cf3e8c5b247224c0e0aa5a6b89ef2bb0
عبدالله مصطفى <161338043+abdallahmustafa23@users.noreply.github.com>
2026-03-04 20:40:53 +0200

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -10,6 +10,8 @@ Employee::Employee(const string& name, const string& password, double salary)
 Employee::Employee(int id,const string& name, const string& password, double salary)
     : Person(id,name, password), salary{}
 {    
+    if (id > nextEmployeeID)
+        nextEmployeeID = id;
     set_salary(salary);
 }
 
@@ -31,6 +33,71 @@ void Employee::display_info() const
 {
     cout << "Employee ID: " << to_string(id) << "\nName: " + name << "\nSalary: " << to_string(salary);
 
+}
+
+void Employee::addClient(Client& client)
+{
+    FilesHelper::saveClient(client);
+    cout << "Client added successfully\n";
+}
+
+Client* Employee::searchClient(int id)
+{
+    vector<Client> clients = FilesHelper::getClients();
+
+    for (int i = 0; i < clients.size(); i++)
+    {
+        if (clients[i].get_id() == id)
+        {
+            return &clients[i];
+        }
+    }
+
+    return nullptr;
+}
+
+void Employee::listClients()
+{
+    vector<Client> clients = FilesHelper::getClients();
+
+    for (int i = 0; i < clients.size(); i++)
+    {
+        clients[i].display_info();
+        cout << endl;
+    }
+}
+
+void Employee::editClient(int id,const string& name,const string& password, double balance)
+{
+    vector<Client> clients = FilesHelper::getClients();
+    bool found = false;
+
+    for (int i = 0; i < clients.size(); i++)
+    {
+        if (clients[i].get_id() == id)
+        {
+            clients[i].set_name(name);
+            clients[i].set_password(password);
+            clients[i].set_balance(balance);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Client not found\n";
+        return;
+    }
+
+    FilesHelper::clearFile("Clients.txt", "LastClientId.txt");
+
+    for (int i = 0; i < clients.size(); i++)
+    {
+        FilesHelper::saveClient(clients[i]);
+    }
+
+    cout << "Client updated successfully\n";
 }
 
 int Employee::nextEmployeeID = 0;
