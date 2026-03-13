aa18c10ad2ce88117099e181e09f5eff5395a15c
عبدالله مصطفى <161338043+abdallahmustafa23@users.noreply.github.com>
2026-03-14 01:07:18 +0200

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,10 +1,8 @@
 #pragma once
-#include "Person.h"
 #include <iostream>
 #include <string>
-#include <vector>
-#include "Client.h";
-#include "FileManager.h"
+#include "Person.h"
+#include "Client.h"
 using namespace std;
 
 class Employee : public Person
@@ -17,7 +15,7 @@ protected:
 
 public:
     Employee(const string& name, const string& password, double salary);
-    Employee(int id,const string& name, const string& password, double salary);
+    Employee(int id, const string& name, const string& password, double salary);
 
     bool set_salary(double salary);
     double get_salary() const;
@@ -25,8 +23,10 @@ public:
     void display_info() const;
 
     void addClient(Client& client);
-    Client* searchClient(int id);
-    void listClients();
-    void editClient(int id,const string& name,const string& password, double balance);
 
+    Client searchClient(int id);
+
+    void listClients();
+
+    void editClient(int id, const string& name, const string& password, double balance);
 };
