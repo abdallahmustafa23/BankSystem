c3d8d51da596585045e0699aa7478cb93ddeaeef
عبدالله مصطفى <161338043+abdallahmustafa23@users.noreply.github.com>
2026-03-14 01:03:37 +0200

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -1,31 +1,34 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include <vector>
+
 #include "Employee.h"
-#include "Client.h";
-#include "FileManager.h"
+
 using namespace std;
 
 class Admin : public Employee
 {
-   static int nextAdminID;
+    static int nextAdminID;
 public:
     Admin(const string& name, const string& password, double salary);
-    Admin(int id,const string& name, const string& password, double salary);
+    Admin(int id, const string& name, const string& password, double salary);
 
     void display_info() const;
 
     void addClient(Client& client);
 
-    Client* searchClient(int id);
+    Client searchClient(int id);
 
     void listClient();
-    void editClient(int id, const string& name,const string& password, double balance);
+
+    void editClient(int id, const string& name, const string& password, double balance);
 
     void addEmployee(Employee& employee);
-    Employee* searchEmployee(int id);
-    void editEmployee(int id, const string& name, const string& password, double salary);
-    void listEmployee()
 
+    Employee searchEmployee(int id);
+
+    void editEmployee(int id, const string& name, const string& password, double salary);
+
+    void listEmployee();
 };
+
+
+
