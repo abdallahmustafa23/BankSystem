0c325bd02598bf08a65b9ba59eb748025a042c59
عبدالله مصطفى <161338043+abdallahmustafa23@users.noreply.github.com>
2026-03-14 01:08:20 +0200

    Update FileManager.h

--- FileManager.h
+++ FileManager.h
@@ -1,24 +1,34 @@
 #pragma once
+#include <iostream>
+#include <string>
+
 #include "DataSourceInterface.h"
 #include "FilesHelper.h"
 
 class FileManager : public DataSourceInterface {
 
 public:
+   
+    static void addClient(const Client& obj);
 
-    void addClient(const Client& obj);
-    void addEmployee(const Employee& obj) ;
-    void addAdmin(const Admin& obj) ;
-
-
-    void getAllClients() ;
-    void getAllEmployees() ;
-    void getAllAdmins() ;
+    static void addEmployee(const Employee& obj);
+     
+    static void addAdmin(const Admin& obj);
 
 
-    void removeAllClients() ;
-    void removeAllEmployees() ;
-    void removeAllAdmins() ;
+    static void getAllClients();
+     
+    static void getAllEmployees();
+
+    static void getAllAdmins();
+
+    static void removeAllClients();
+
+    static void removeAllEmployees();
+           
+    static void removeAllAdmins();
 
 };
+
+
 
