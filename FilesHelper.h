469e877bf7d84566b7d50efba0d6cf2608220166
عبدالله مصطفى <161338043+abdallahmustafa23@users.noreply.github.com>
2026-03-14 01:09:08 +0200

    Update FilesHelper.h

--- FilesHelper.h
+++ FilesHelper.h
@@ -1,6 +1,7 @@
 #pragma once
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
 #include "Client.h"
 #include "Employee.h"
@@ -9,23 +10,29 @@
 using namespace std;
 
 class FilesHelper {
-public:    
+public:
+
     static void saveLast(const string& fileName, int id);
+
+
     static int getLast(const string& fileName);
 
     //id|name|password|balance
-    static void saveClient(const Client& c, const string& fileName = "Clients.txt", const string& lastIdFile = "LastClientId.txt");
+    static void saveClient(const Client& c);
 
     //id|name|password|salary
     static void saveEmployee(const Employee& e, const string& fileName = "Employees.txt", const string& lastIdFile = "LastEmployeeId.txt");
 
     //id|name|password|balance
-    static void saveAdmin(const Admin& a, const string& fileName = "Admins.txt", const string& lastIdFile = "LastAdminId.txt") ;
+    static void saveAdmin(const Admin& a, const string& fileName = "Admins.txt", const string& lastIdFile = "LastAdminId.txt");
 
 
-    static vector<Client> getClients(const string& fileName = "Clients.txt") ;
-    static vector<Employee> getEmployees(const string& fileName = "Employees.txt") ;
-    static vector<Admin> getAdmins(const string& fileName = "Admins.txt") ;
-    
-    static void clearFile(const string& fileName, const string& lastIdFile) ;
+    static vector<Client> getClients(const string& fileName = "Clients.txt");
+
+
+    static vector<Employee> getEmployees(const string& fileName = "Employees.txt");
+
+    static vector<Admin> getAdmins(const string& fileName = "Admins.txt");
+
+    static void clearFile(const string& fileName, const string& lastIdFile);
 };
